b4e9a7e6b2ec7de08f7f1cf8b84518abb1eb2e13
willslaq <willslaqtomaz@gmail.com>
2021-07-14 00:15:14 -0300

    estruturado estiulização e instalado libs

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+  
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './screens/Login';
+import Developers from './screens/Developers';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/cart" component={Developers} />
+      </Switch>
+    </BrowserRouter>
+  );
+}

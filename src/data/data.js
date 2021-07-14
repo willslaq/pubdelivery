bb0bad152037877558dbe59677e3fd52f8b60c60
willslaq <willslaqtomaz@gmail.com>
2021-07-14 02:22:32 -0300

    Adicionado mais um produto no data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,5 @@
 export const data = [
   {id: 1, description: 'Skol 269ml', price: 2.99, packValue: 37.00},
   {id: 2, description: 'Brahma Duplo Malte', price: 3.49, packValue: 40.99},
+  {id: 3, description: 'Brahma 350ml', price: 2.99, packValue: 29.99},
 ]
